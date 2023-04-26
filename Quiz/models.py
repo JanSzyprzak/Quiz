606895bacf7877350dd0c65e91fd0355be4142c0
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-04-26 09:41:58 +0200

    Add models

--- Quiz/models.py
+++ Quiz/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)  
+    
+    def __str__(self):
+        return self.name
+
+class QuizResult(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100)
+    difficulty = models.CharField(max_length=20)
+    points = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user} - {self.subject} - {self.difficulty} - {self.points}"
+    
+class Question(models.Model):
+    text = models.CharField(max_length=200)
+    subject = models.CharField(max_length=100)
+    difficulty = models.CharField(max_length=20)
+    answer = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.subject} - {self.difficulty} - {self.text}"
