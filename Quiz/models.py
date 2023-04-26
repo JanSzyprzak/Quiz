f3fd8c688b874b5bd7dd6840f4fdd0b7732e459e
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-04-26 10:47:20 +0200

    Modify  models

--- Quiz/models.py
+++ Quiz/models.py
@@ -1,28 +1,51 @@
 from django.db import models
 
 
-class User(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)  
-    
+
     def __str__(self):
         return self.name
 
-class QuizResult(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    subject = models.CharField(max_length=100)
-    difficulty = models.CharField(max_length=20)
+
+class Question(models.Model):
+    text = models.CharField(max_length=200)
+    answer = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     points = models.IntegerField()
 
     def __str__(self):
-        return f"{self.user} - {self.subject} - {self.difficulty} - {self.points}"
-    
-class Question(models.Model):
-    text = models.CharField(max_length=200)
-    subject = models.CharField(max_length=100)
-    difficulty = models.CharField(max_length=20)
-    answer = models.CharField(max_length=100)
+        return f"{self.category}: {self.text}"
+
+
+class Quiz(models.Model):
+    DIFFICULTY_CHOICES = [
+        ('easy', 'Easy'),
+        ('medium', 'Medium'),
+        ('hard', 'Hard'),
+    ]
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    difficulty = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES)
+    result = models.IntegerField()
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.subject} - {self.difficulty} - {self.text}"
+        return f"{self.category} - {self.difficulty} - {self.result}"
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Leaderboard(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user} - {self.score}"
