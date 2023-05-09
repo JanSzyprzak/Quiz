f8039796fe9c19cd417c0acbb06f8dbbf3136b71
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-05-09 10:47:06 +0200

    Add get_data() returning questions from ext API

--- Quiz/views.py
+++ Quiz/views.py
@@ -1,3 +1,17 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+
+def get_data(request):
+    url = 'https://opentdb.com/api.php?amount=10&category=22&difficulty=easy'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data["results"]
+    else:
+        return None
+
