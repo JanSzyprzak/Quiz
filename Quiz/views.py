607341a9cd6b8d64ad68978e21adb1f3bf7a2ed3
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-05-11 10:53:55 +0200

    Refactor fetch_data function

--- Quiz/views.py
+++ Quiz/views.py
@@ -1,17 +1,26 @@
 import requests
+import random
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
 
 
 
-
-def get_data(request):
-    url = 'https://opentdb.com/api.php?amount=10&category=22&difficulty=easy'
+def fetch_data(request):
+    url = 'https://opentdb.com/api.php?amount=10'
     response = requests.get(url)
+
     if response.status_code == 200:
         data = response.json()
-        return data["results"]
-    else:
-        return None
 
+        for question in data['results']:
+            answers = question['incorrect_answers']
+            answers.append(question['correct_answer'])
+            random.shuffle(answers)
+            question['mixed_answers'] = answers
+
+        return render(request, 'Quiz/data.html', {'data': data})
+    else:
+        return render(request, 'Quiz/data.html', {'data': None})
