3abe387dcfebdb9a8f45f4100e68b09b6b894806
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-05-13 17:52:27 +0200

    Add paths for login and logout

--- Quiz/urls.py
+++ Quiz/urls.py
@@ -5,10 +5,14 @@ from django.contrib.auth import views as auth_views
 app_name = 'Quiz'
 
 urlpatterns = [
-    path('', views.fetch_data, name='quiz'),
+    path('quiz', views.fetch_data, name='quiz'), 
     path('submit/', views.submit_answers, name='submit_answers'),
     path('result/', views.result, name='result'),
     path('login/', auth_views.LoginView.as_view(template_name='Quiz/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('register/', views.register, name='register'),
+    path('', views.quiz_config, name='quiz_config'),
 ]
+
+
+    
